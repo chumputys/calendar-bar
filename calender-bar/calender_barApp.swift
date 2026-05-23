24e64ca0807a67fa31bc0d7b5e9c9f66abc866c4
Ubaidulla Ali <chump0210@gmail.com>
2026-05-23 19:11:00 +0800

    Initial Commit

---
+++ calender-bar/calender_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  calender_barApp.swift
+//  calender-bar
+//
+//  Created by Ubaidulla Ali on 23-5-26.
+//
+
+import SwiftUI
+
+@main
+struct calender_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
